8d78b05b6096689f28b059bd0d0b5b4e30498935
Caleb Mueller <mc61@hawaii.edu>
2023-03-19 11:28:09 -1000

    Removed internal functions from switch.h interface, and added a 'next' element to the TableEntry struct.  Beginning to alter routing table to be an arrayed linked list to enable multiple hosts to be associated with a single port

--- include/switch.h
+++ include/switch.h
@@ -13,11 +13,7 @@ struct Job;
 struct TableEntry {
   bool isValid;
   int id;
+  struct TableEntry *next;
 };
-
-int searchRoutingTableForValidID(struct TableEntry *rt, int id);
-
-void broadcastToAllButSender(struct Job *job, struct TableEntry *rt,
-                             struct Net_port **port_array, int port_array_size);
 
 void switch_main(int switch_id);
